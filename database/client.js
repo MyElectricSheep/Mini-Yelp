5b0c1659b32e395ab2b7e82cefc1a4d6f54139be
Ben Paget <ben.paget@live.fr>
2021-07-16 18:47:13 +0200

    Update database setup

--- database/client.js
+++ database/client.js
@@ -5,6 +5,9 @@ let pool;
 if (process.env.NODE_ENV === "production") {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   });
 } else {
   pool = new Pool();
